9c0ead7cc044c6933f771a0a463ad5fb89d18d8c
rae-glazier <glazier.home.life@gmail.com>
2020-04-26 20:59:35 -0500

    get id complete

--- api/sql-router.js
+++ api/sql-router.js
@@ -5,11 +5,32 @@ const router = express.Router()
 
 router.get("/", async (req, res, next) => {
     try{
-        const customers = await db.select("*").from("accounts")
-        res.json(customers)
+        const accounts = await db.select("*").from("accounts")
+        res.json(accounts)
     } catch (err) {
         next(err)
     }
+})
+
+router.get("/:id", async (req, res, next) => {
+    try{
+        const accounts = await db("accounts").where("id", req.params.id).first()
+        res.json(accounts)
+    } catch (err) {
+        next (err)
+    }
+})
+
+router.post("/", async (req, res, next) => {
+
+})
+
+router.put("/", async (req, res, next) => {
+
+})
+
+router.delete("/:id", (req, res, next) => {
+
 })
 
 module.exports = router
