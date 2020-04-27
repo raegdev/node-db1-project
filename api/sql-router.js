af2a359b49ddb0ed7fcced211315f5ece01fea8c
rae-glazier <glazier.home.life@gmail.com>
2020-04-26 21:07:53 -0500

    delete crud completed

--- api/sql-router.js
+++ api/sql-router.js
@@ -35,12 +35,27 @@ router.post("/", async (req, res, next) => {
     }
 })
 
-router.put("/", async (req, res, next) => {
-
+router.put("/:id", async (req, res, next) => {
+    try{
+        const payload = {
+            name: req.body.name,
+            budget: req.body.budget
+        }
+        await db("accounts").where("id", req.params.id).update(payload)
+        const updatedAccount = await db("accounts").where("id", req.params.id).first()
+        res.json(updatedAccount)
+    } catch (err) {
+        next (err)
+    }
 })
 
-router.delete("/:id", (req, res, next) => {
-
+router.delete("/:id", async (req, res, next) => {
+    try{
+        await db("accounts").where("id", req.params.id).del()
+        res.status(204).end()
+    } catch (err) {
+        next (err)
+    }
 })
 
 module.exports = router
